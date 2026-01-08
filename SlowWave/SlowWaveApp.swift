b451be10332bb24e3993119d30cdc72c0eaff2f9
Haoze <luoshifenw@gmail.com>
2026-01-08 09:33:18 +0800

    Initial Commit

---
+++ SlowWave/SlowWaveApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlowWaveApp.swift
+//  SlowWave
+//
+//  Created by 吴素 on 1/8/26.
+//
+
+import SwiftUI
+
+@main
+struct SlowWaveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
